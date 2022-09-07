25a029cd7e6d03c9bd2fc716ad5137032115fb6d
BaraSec <barasec@protonmail.com>
2022-09-08 00:01:57 +0400

    Removed eslint issue. Hotel page & route init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Hotels from './Pages/Hotels'
+import Hotel from './Pages/Hotel'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Hotels' element={<Hotels />} />
+        <Route path='/Hotels:id' element={<Hotel />} />
       </Routes>
     </Router>
   )
