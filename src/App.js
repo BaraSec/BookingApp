8e18b8fa6c3c6690d668a20d866151efeebc80cb
BaraSec <barasec@protonmail.com>
2022-09-08 01:23:03 +0400

    navbar & header init setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Hotels from './Pages/Hotels'
-import Hotel from './Pages/Hotel'
+import Home from './Pages/Home/Home'
+import Hotels from './Pages/Hotels/Hotels'
+import Hotel from './Pages/Hotel/Hotel'
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Hotels' element={<Hotels />} />
-        <Route path='/Hotels:id' element={<Hotel />} />
+        <Route path='/Hotels/:id' element={<Hotel />} />
       </Routes>
     </Router>
   )
