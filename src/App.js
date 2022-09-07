964290dcfd8f6204372567f94f9e57d54ad81470
BaraSec <barasec@protonmail.com>
2022-09-07 23:53:33 +0400

    Project Setup Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Hotels from './Pages/Hotels'
+
+const App = () => {
   return (
-    <div>
-      Hello world!
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Hotels' element={<Hotels />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
